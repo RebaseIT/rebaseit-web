35715b7f26fd672881d5081dd3aa441c6feaa99d
Jorge Covello <141249841+jcovello-rebase@users.noreply.github.com>
2024-05-20 23:52:46 -0300

    add ssr false property

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
   },
+  ssr: false,
   app: {
     head: {
       link: [{ rel: 'icon', type: 'image/png', href: "/images/favicon.png" }]
