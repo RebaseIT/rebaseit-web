dee664a4d76bce325782fad7aa731c50fd33ceb5
jcueli-rebase <juan.cueli@rebaseit.tech>
2024-06-18 21:16:54 -0300

    Using new photos

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -15,33 +15,28 @@ const image = {
 const teamOptions = [
   {
     name: 'Juan Cueli',
-    image: '/images/team/juan-cueli.png',
-    alt: 'juan-cueli',
-    class: 'space-layout-sm'
+    image: '/images/team/juan.png',
+    alt: 'juan',
   },
   {
     name: 'Ezequiel Díaz',
-    image: '/images/team/ezequiel-diaz.png',
-    alt: 'ezequiel-diaz',
-    class: 'space-layout-md'
+    image: '/images/team/ezequiel.png',
+    alt: 'ezequiel',
   },
   {
     name: 'Rodrigo Díaz',
-    image: '/images/team/rodrigo-diaz.png',
-    alt: 'rodrigo-diaz',
-    class: 'space-layout-lg'
+    image: '/images/team/rodrigo.png',
+    alt: 'rodrigo',
   },
   {
     name: 'Martín Urteaga',
-    image: '/images/team/martin-urteaga.png',
-    alt: 'martin-urteaga',
-    class: 'space-layout-md'
+    image: '/images/team/martin.png',
+    alt: 'martin'
   },
   {
     name: 'Jorge Covello',
-    image: '/images/team/jorge-covello.png',
-    alt: 'jorge-covello',
-    class: 'space-layout-smspace-layout-md'
+    image: '/images/team/jorge.png',
+    alt: 'jorge'
   }
 ];
 
@@ -163,15 +158,15 @@ const polygons = [
         :class="{ 'mt-6': isMobile }"
       >
         <div
-          class="flex flex-column align-items-center w-fit"
+          class="flex flex-column align-items-center w-fit mt-4"
           v-for="(member, index) in teamOptions"
           :key="`member-${index}`"
         >
           <ReImage
             :alt="member.alt"
             :src="member.image"
-            width="178px"
-            height="205px"
+            width="150px"
+            height="242px"
             :class="isMobile ? 'mt-0' : member.class"
           />
           <ReTitleSpan
