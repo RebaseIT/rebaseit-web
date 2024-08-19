2f89cd07a2a1e7265a451f68ace8ad2fbaa4fe65
Jorge Covello <141249841+jcovello-rebase@users.noreply.github.com>
2024-08-19 17:07:46 -0300

    remove commented code and unused regex

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
-const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 export default defineNuxtConfig({
-  runtimeConfig: {
-    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY
-  },
-  // vite: {
-  //   build: {
-  //     rollupOptions: {
-  //       output: {
-  //         // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
-  //         sanitizeFileName(name) {
-  //           const match = DRIVE_LETTER_REGEX.exec(name);
-  //           const driveLetter = match ? match[0] : "";
-  //           return (
-  //             driveLetter +
-  //             name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
-  //           );
-  //         },
-  //       },
-  //     },
-  //   },
-  // },
   ssr: false,
   app: {
     head: {
