9506d708d7e9a200a746538d0ea07d000c062909
ediaz-rebase <152353835+ediaz-rebase@users.noreply.github.com>
2025-01-02 18:05:51 +0000

    Deploying to gh-pages from @ RebaseIT/rebaseit-web@b695869259e700256a4c0f3d46f488a6da852a92 🚀

---
+++ _nuxt/BTwilsKe.js
@@ -0,0 +1,1 @@
+import{_ as o,k as n,o as s,g as _}from"./CU6Z0vau.js";const c={__name:"ReButton",props:{type:{type:String,default:"button",validator:t=>["submit","reset","button"].includes(t)}},setup(t){return(a,p)=>{const e=n("Button");return s(),_(e,{type:t.type,class:"re-button",size:"small"},null,8,["type"])}}},u=o(c,[["__scopeId","data-v-361dcd13"]]);export{u as _};
